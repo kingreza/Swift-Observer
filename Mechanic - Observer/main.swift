16bf743c43abf7db8a255f630ba67721989ce8ef
Reza Shirazian <reza.shirazian@gmail.com>
2016-04-08 00:34:45 -0700

    Initial Commit

---
+++ Mechanic - Observer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Mechanic - Observer
+//
+//  Created by Reza Shirazian on 2016-04-08.
+//  Copyright © 2016 Reza Shirazian. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
